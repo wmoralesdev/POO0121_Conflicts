5a4ea3e5742f2859b91d20366b30b1163eff7d4c
Walter Morales <walterrafael26@gmail.com>
2021-03-15 19:18:12 -0600

    Add terminal test 2

---
+++ TerminalTest2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TerminalTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estoy en TerminalTest2");
+        }
+    }
+}
